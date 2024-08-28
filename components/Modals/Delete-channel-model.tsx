8582a48e95f4f25cdb3bdd496ff13048359d9455
Krishna <ikrishnak0203@gmail.com>
2024-08-28 21:47:43 +0530

    eslint configure

--- components/Modals/Delete-channel-model.tsx
+++ components/Modals/Delete-channel-model.tsx
@@ -56,7 +56,7 @@ const DeleteChannel = () => {
       <DialogContent className="bg-white  text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Delete "{channel?.name}" Channel
+            Delete &quot;{channel?.name}&quot; Channel
           </DialogTitle>
           <DialogDescription className="text-center text-md">
             Are you Sure you want to Delete <span className="font-semibold text-indigo-500">#{channel?.name}</span> Channel
